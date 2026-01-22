222447d5a0871901c8284e9df723362669329411
bakaruu <arucastro@gmail.com>
2026-01-22 13:38:37 +0100

    Find One User Done and Working.

--- src/main/java/com/rest/webservices/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -22,5 +23,21 @@ public class UserDaoService {
     public List<User> findAll(){
         return users;
     }
+
+
+    public User findOne(int id) {
+        for (User user : users) {
+            if (user.getId().equals(id)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    // Functional programming
+//    public User findOne(int id ){
+//        Predicate<? super User> predicate = user -> user.getId().equals(id);
+//        return users.stream().filter(predicate).findFirst().get();
+//    }
 
 }
--- src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,6 +1,7 @@
 package com.rest.webservices.restful_web_services.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.PublicKey;
@@ -20,5 +21,15 @@ public class UserResource {
     public List<User> retrieveAllUsers(){
 
         return service.findAll();
+
+
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUsers(@PathVariable int id){
+
+        return service.findOne(id);
+
+
     }
 }
