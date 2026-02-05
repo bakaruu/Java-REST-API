3ae4634b3576b63ed1b267b504b07f2276592133
bakaruu <arucastro@gmail.com>
2026-02-05 19:50:15 +0100

    Post user with ResponseEntity + location.

--- src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.rest.webservices.restful_web_services.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.security.PublicKey;
 import java.util.List;
 
@@ -32,7 +35,16 @@ public class UserResource {
 
     //POST /users
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
