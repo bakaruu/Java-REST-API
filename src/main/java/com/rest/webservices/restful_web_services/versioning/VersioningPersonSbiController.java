ebf3f3af4fe9c3dfe2a1d63f3c0a477d4ce00b8e
bakaruu <arucastro@gmail.com>
2026-03-10 04:38:59 +0100

    Versioning using Spring Boot Versioning

---
+++ src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonSbiController.java
@@ -0,0 +1,69 @@
+package com.rest.webservices.restful_web_services.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//public class VersioningPersonSbiController {
+//
+//
+//    //URI VERSIONING
+////    @GetMapping(value = "/{version}/personsbi", version = "1.0.0")
+////    public PersonV1 getFirstVersionOfPerson(){
+////        return new PersonV1("Bob Charlie");
+////
+////    }
+////
+////
+////    @GetMapping(value = "/{version}/personsbi", version = "2.0.0")
+////    public PersonV2 getSecondVersionOfPerson(){
+////        return new PersonV2(new Name("Bob", "daniels"));
+////
+////    }
+//
+//    //Request Param VERSIONING
+//
+//    @GetMapping(path = "/personsbi", version = "1.0.0")
+//    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+//        return new PersonV1("Bob Charlie");
+//
+//    }
+//
+//    @GetMapping(path = "/personsbi", version = "2.0.0")
+//    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+//        return new PersonV2(new Name("Bob", "daniels"));
+//
+//    }
+//
+//    //Headers VERSIONING
+//
+//    @GetMapping(path = "/personsbi/header", headers = "X-API-VERSION=1")
+//    public PersonV1 getFirstVersionOfPersonRequestHeader(){
+//        return new PersonV1("Bob Charlie");
+//
+//    }
+//
+//    @GetMapping(path = "/personsbi/header", headers = "X-API-VERSION=2")
+//    public PersonV2 getSecondVersionOfPersonRequestHeader(){
+//        return new PersonV2(new Name("Bob", "daniels"));
+//
+//    }
+//
+//
+//    //Media type VERSIONING
+//
+//
+//    @GetMapping(path = "/personsbi/accept", produces = "application/vnd.company.app-v1+json")
+//    public PersonV1 getFirstVersionOfPersonAcceptHeader(){
+//        return new PersonV1("Bob Charlie");
+//
+//    }
+//
+//    @GetMapping(path = "/personsbi/accept", produces = "application/vnd.company.app-v2+json")
+//    public PersonV2 getSecondVersionOfPersonAcceptHeader(){
+//        return new PersonV2(new Name("Bob", "daniels"));
+//
+//    }
+//
+//
+//}
