b330271a17079fdb4e1f887e78acdd725a6f6df1
bakaruu <arucastro@gmail.com>
2026-03-10 03:25:02 +0100

    Versioning REST API - Request Param, Header and Content Negotiation

--- src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+
+    //URI VERSIONING
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson(){
         return new PersonV1("Bob Charlie");
@@ -18,5 +20,50 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Bob", "daniels"));
 
     }
+
+    //Request Param VERSIONING
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("Bob Charlie");
+
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2(new Name("Bob", "daniels"));
+
+    }
+
+    //Headers VERSIONING
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader(){
+        return new PersonV1("Bob Charlie");
+
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader(){
+        return new PersonV2(new Name("Bob", "daniels"));
+
+    }
+
+
+    //Media type VERSIONING
+
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader(){
+        return new PersonV1("Bob Charlie");
+
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader(){
+        return new PersonV2(new Name("Bob", "daniels"));
+
+    }
+
 
 }
