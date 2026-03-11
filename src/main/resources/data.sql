0c010eeccb55d6b5670ef7ecc727f1969e8f9aa1
bakaruu <arucastro@gmail.com>
2026-03-11 03:54:25 +0100

    Creating Post Entity with Many to One Relationship with User Entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO user_details (id, name, birth_date) VALUES (1, 'Adam', '1990-05-15');
 INSERT INTO user_details (id, name, birth_date) VALUES (2, 'Eve', '1985-03-22');
 INSERT INTO user_details (id, name, birth_date) VALUES (3, 'Carlos', '1995-11-08');
+INSERT INTO post (id, description, user_id) VALUES (4, 'WTF?', 1);
